34ef11d80c970e3482a8a1f6e01c780b3b49bb08
justinChang <c24971720@gmail.com>
2024-11-16 15:22:57 +0800

    add manual widget

---
+++ ManualWidget/ViewManual.h
@@ -0,0 +1,44 @@
+#ifndef VIEW_YML_DISPLAY_H
+#define VIEW_YML_DISPLAY_H
+
+#include <QtCore>
+#include <QtWidgets>
+
+#include <QWidget>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#include "md2html.h"
+
+class ViewManual : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ViewManual(QWidget *parent = nullptr);
+
+    QTextEdit*  teManual;
+
+private:
+    QHBoxLayout* hlytToolbar;
+    QComboBox* cbbSearchBar;
+    QPushButton* btnSearch;
+    QHBoxLayout* hlytManualMain;
+    QVBoxLayout* vlytManualTitle;
+    QListWidget* lwManualTitle;
+    QVBoxLayout* vlytManualContent;
+
+    QStackedWidget* stkwManualPages;
+
+    QWidget* addManualPage(const string& btnText);
+    void Widgets();
+    void Layouts();
+
+
+private slots:
+    void handleListWidgetItemClick(QListWidgetItem *item);
+
+};
+
+#endif /* VIEW_YML_DISPLAY_H */
