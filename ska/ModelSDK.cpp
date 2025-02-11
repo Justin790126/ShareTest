4d2d40ba744d322f6086a54750a6aef7f0ee22d7
JustinChang <c24971720@gmail.com>
2025-02-11 22:11:50 +0800

    add basic fork

--- ska/ModelSDK.cpp
+++ ska/ModelSDK.cpp
@@ -7,5 +7,20 @@ ModelSDK::ModelSDK(QObject *parent)
 
 void ModelSDK::run()
 {
-    cout << "run" << endl;
+    pid_t child_pid = fork();
+    cout << "child_pid: " << child_pid << endl;
+    if (child_pid <0) {
+        cout << "Failed to fork" << endl;
+        return;
+    } else if (child_pid == 0) {
+        ModelSktSvr svr;
+        svr.init();
+        svr.start();
+        svr.Close();
+        cout << "EOF Child process: " << child_pid << endl;
+        exit(0);
+    } else {
+        waitpid(child_pid,NULL,0);
+    }
+    cout << "EOF SDK svr: " << child_pid << endl;
 }
--- ska/ModelSDK.h
+++ ska/ModelSDK.h
@@ -4,16 +4,23 @@
 #include <QThread>
 #include <iostream>
 
+#include "ModelSktMsg.h"
+#include "ModelSktSvr.h"
+
 using namespace std;
 
 class ModelSDK : public QThread
 {
     Q_OBJECT
-    public:
+public:
     ModelSDK(QObject *parent = 0);
     ~ModelSDK() = default;
-    protected:
+
+protected:
     virtual void run() override;
+
+private:
+    ModelSktMsg* m_svr = NULL;
 };
 
 #endif /* MODEL_SDK_H */
