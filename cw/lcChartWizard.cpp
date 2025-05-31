8c9495df5801a6a2673969d99c899fd88d806c3c
JustinChang <c24971720@gmail.com>
2025-05-31 12:40:34 +0800

    add data structure for chart

--- cw/lcChartWizard.cpp
+++ cw/lcChartWizard.cpp
@@ -1,7 +1,5 @@
 #include "lcChartWizard.h"
 
-#include "data.h"
-
 lcChartWizard::lcChartWizard(QWidget* parent)
     : QWidget(parent)
 {
@@ -9,7 +7,48 @@ lcChartWizard::lcChartWizard(QWidget* parent)
 
     resize(640, 480);
 
+    vector<double> x_nms(SLICE_DATA.size());
+    double nmPerStep = 4;
+    for (int i = 0; i < SLICE_DATA.size(); ++i)
+    {
+        x_nms[i] = i*nmPerStep; // Assuming x-axis is just the index
+    }
+    vector<double> y_noise(SLICE_DATA.size());
+    for (int i = 0; i < SLICE_DATA.size(); ++i)
+    {
+        // add random noise
+        y_noise[i] = SLICE_DATA[i] + ((rand() % 100) / 10000.0); // Adding small random noise
+    }
 
+    // draw line chart
+    // m_qcp->addGraph();
+    // m_qcp->graph(0)->setData(QVector<double>::fromStdVector(x_nms), QVector<double>::fromStdVector(y_noise));
+    // m_qcp->xAxis->setLabel("X Axis (nm)");
+    // m_qcp->yAxis->setLabel("Y Axis (arbitrary units)");
+    // m_qcp->xAxis->setRange(0, x_nms.back());
+    // m_qcp->yAxis->setRange(*std::min_element(y_noise.begin(), y_noise.end()), *std::max_element(y_noise.begin(), y_noise.end()));
+    // m_qcp->replot();
+    // // let plot selectable
+    // m_qcp->setInteractions(QCP::iSelectPlottables | QCP::iSelectAxes | QCP::iRangeDrag | QCP::iRangeZoom);
+
+    // // change line color
+    // m_qcp->graph(0)->setPen(QPen(Qt::yellow, 2)); // Set line color to blue and width to 2
+    // // change line brush
+    // m_qcp->graph(0)->setBrush(QBrush(QColor(255, 255, 0, 50))); // Set fill color with some transparency
+    // // scatter points with circle
+    // m_qcp->graph(0)->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 5)); // Set scatter style to circle with radius 5
+    // // set name
+    // m_qcp->graph(0)->setName("Line Chart ExampleQQ");
+    // // set title
+    // // m_qcp->plotLayout()->insertRow(0);
+    // // m_qcp->plotLayout()->addElement(0, 0, new QCPTextElement(m_qcp, "Line Chart Example", QFont("sans", 12, QFont::Bold)));
+    // // set legend
+    // m_qcp->legend->setVisible(true);
+    // m_qcp->legend->setFont(QFont("sans", 10));
+    // m_qcp->legend->setBrush(QBrush(QColor(255, 255, 255, 200))); // Set legend background color with some transparency
+    // update y axis data
+    // m_qcp->graph(0)->setData(QVector<double>::fromStdVector(x_nms), QVector<double>::fromStdVector(SLICE_DATA));
+    // m_qcp->replot();
 }
 
 lcChartWizard::~lcChartWizard()
@@ -24,16 +63,23 @@ void lcChartWizard::Widgets()
 
 }
 
-QWidget* lcChartWizard::CreateLineChartProps()
+QWidget* lcChartWizard::CreateLineChartProps(ChartInfo& chartInfo)
 {
     ViewLineChartProps* section = new ViewLineChartProps("Line Chart Properties");
     {
         QVBoxLayout* vlyt = new QVBoxLayout;
-        
         vlyt->addWidget(new QLabel("Line Chart Properties", this));
-
         section->setContentLayout(*vlyt);
     }
+
+    // create graph on m_qcp and set graph index to chartInfo, and plot it
+    chartInfo.SetQCustomPlot(m_qcp);
+    chartInfo.SetGraphIndex(m_qcp->graphCount());
+    m_qcp->addGraph();
+
+    int graphIndex;
+    graphIndex = chartInfo.GetGraphIndex();
+
     return section;
 }
 
--- cw/lcChartWizard.h
+++ cw/lcChartWizard.h
@@ -4,6 +4,81 @@
 #include <QtGui>
 #include "ViewLineChartProps.h"
 #include "qcustomplot.h"
+#include <iostream>
+
+#include "data.h"
+
+
+using namespace std;
+
+const extern std::vector<double> SLICE_DATA;
+
+/*
+    * ChartInfo
+    * This class is responsible for storing information about a chart.
+    * It includes methods to set the x and y data, pen, brush, and graph index.
+*/
+class ChartInfo
+{
+public:
+    ChartInfo() = default;
+    ~ChartInfo() = default;
+    void SetX(const QVector<double>& qvX)
+    {
+        m_qvX = qvX;
+    }
+    void SetY(const QVector<double>& qvY)
+    {
+        m_qvY = qvY;
+    }
+    void SetPen(const QPen& pen)
+    {
+        m_pen = pen;
+    }
+    void SetBrush(const QBrush& brush)
+    {
+        m_brush = brush;
+    }
+    void SetGraphIndex(int idx)
+    {
+        m_iGphIdx = idx;
+    }
+    int GetGraphIndex() const
+    {
+        return m_iGphIdx;
+    }
+    void SetQCustomPlot(QCustomPlot* pQCP)
+    {
+        m_pQCP = pQCP;
+    }
+    QCustomPlot* GetQCustomPlot() const
+    {
+        return m_pQCP;
+    }
+
+
+    friend ofstream& operator<<(std::ofstream& ofs, const ChartInfo& info)
+    {
+        
+        return ofs;
+    }
+private:
+    QVector<double> m_qvX;
+    QVector<double> m_qvY;
+
+    QString m_sLegendName;
+
+    QPen m_pen;
+    QBrush m_brush;
+    int m_iGphIdx;
+    QCustomPlot* m_pQCP = NULL;
+};
+
+/*
+    * lcChartWizard
+    * This class is responsible for creating a chart wizard interface.
+    * It includes methods to create line chart properties and manage the layout.
+*/
 
 class lcChartWizard : public QWidget
 {
@@ -11,7 +86,7 @@ class lcChartWizard : public QWidget
 public:
     lcChartWizard(QWidget* parent = nullptr);
     ~lcChartWizard();
-    QWidget* CreateLineChartProps();
+    QWidget* CreateLineChartProps(ChartInfo& chartInfo);
     QVBoxLayout* vlytLeft;
 private:
     void Widgets();
